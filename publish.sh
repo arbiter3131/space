5a63ff5e54659b102e5ae2eb912dcdb18f7e38aa
Julien <julienlacour31@gmail.com>
2020-11-05 11:20:37 +0200

    Add publishing template

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@ printf "%s" "$LICENSE_KEY" > ope/licensekey.txt
 # Replace all the '@@' characters back to '\n' in the 'licensekey.txt' file
 sed -i 's/@@/\n/g' ope/licensekey.txt
 # Run the PDF CSS HTML5 transformation
-ope/bin/dita -f pdf-css-html5 -i sample/space.ditamap -o sample/out/pdf-css-html5
+ope/bin/dita -f pdf-css-html5 -i sample/space.ditamap -o sample/out/pdf-css-html5 -Dpdf.publishing.template=publishing  -Dpdf.publishing.template.descriptor=basket.opt
 # Run the WebHelp Responsive transformation
-ope/bin/dita -f webhelp-responsive -i sample/space.ditamap -o sample/out/webhelp-responsive
+ope/bin/dita -f webhelp-responsive -i sample/space.ditamap -o sample/out/webhelp-responsive -Dwebhelp.publishing.template=publishing  -Dwebhelp.publishing.template.descriptor=basket.opt
